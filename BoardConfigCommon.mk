34cadc250df8103e0d5b38168046eee38e7e8af8
Andrzej Perczak <linux@andrzejperczak.com>
2024-12-06 23:29:46 +0530

    sm8250-common: Don't report memcg functionality in cmdline
    Memcg was disabled in kernel as on Google devices, it is said
    to cause performance regression.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: Ida00530c7f50c0ed33c62714b7e3f6f135de64b8
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,7 +83,6 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
-    androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
     loop.max_part=7 \
